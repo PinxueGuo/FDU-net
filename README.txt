b81ea88d6062a14e723a622edc71db667bc51c1f
PinxueGuo <pxguo21@m.fudan.edu.cn>
2021-12-14 14:05:19 +0800

    initial

---
+++ README.txt
@@ -0,0 +1,27 @@
+复旦大学自动重联网软件
+请按需自己选择以下版本exe使用：
+
+FDU-net.exe		
+按提示输入学号密码，连接成功后将在后台运行。
+
+FDU-net_IDtxt.exe			
+在ID.txt中，第一行写学号，第二行写密码。
+随后每次运行FDU-net.exe，无需输入学号密码。
+
+FDU-net.exe （适用于确定不会断电关机情况 eg: 没在实验室但想卷的日子）保持永远在线。
+FDU-net_IDtxt.exe （适用于可能会断电重启的情况 eg: 寒暑假）+ 自行设置断电重启+此exe开机自启动。
+
+（愿意自己动手的同学，可以用v0_my源码，写死学号密码自行编译打包自己专属exe，无需.txt即可实现和FDU-net_IDtxt.exe相同功能，更优雅？）
+
+
+
+ps：
+需要已安装chrome浏览器，且chrome版本为96.
+包内的chromedriver.exe请保持与当前要使用的FDU-net_v.exe在同一路径下
+开启后首次登录可能不成功，若登录失败会10秒后重连，请耐心等待
+本程序不会记录宁的学号密码
+
+
+
+Github: https://github.com/PinxueGuo/FDU-net
+代码已开源，可以自己改着玩，./score_code
---
+++ source_code/v1_input/FDU-net.py
@@ -0,0 +1,87 @@
+#coding:utf-8
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import requests
+import ctypes
+
+username_str = input('学号：')
+password_str = input('密码：')
+
+whnd = ctypes.windll.kernel32.GetConsoleWindow()
+
+class Login:
+    def login(self):
+        try:
+            # chrome_driver=r"C:\ProgramData\Miniconda3\Lib\site-packages\selenium\chromedriver.exe"
+            chrome_driver=r"chromedriver.exe"
+            # driver = webdriver.Firefox()
+            driver=webdriver.Chrome(executable_path=chrome_driver)
+            driver.get("http://10.108.255.249/srun_portal_pc.php?ac_id=1&&phone=1")
+            time.sleep(1)
+            
+            username_input = driver.find_element_by_name('username')
+            password_input = driver.find_element_by_id('password')
+            login_button = driver.find_element_by_id('button')
+
+            username_input.send_keys(username_str)
+            password_input.send_keys(password_str)
+            login_button.click()
+        except:
+            print(self.getCurrentTime(), u"登陆函数异常")
+        finally:
+            driver.close()
+
+
+    #获取当前时间
+    def getCurrentTime(self):
+        return time.strftime('[%Y-%m-%d %H:%M:%S]',time.localtime(time.time()))
+
+    #判断当前是否可以连网
+    def canConnect(self):
+        try:
+            baidu_request=requests.get("http://www.baidu.com")
+            if(baidu_request.status_code==200):
+                baidu_request.encoding = 'utf-8'
+                baidu_request_bsObj = BeautifulSoup(baidu_request.text, 'html.parser')
+                baidu_input = baidu_request_bsObj.find(value="百度一下")
+                if baidu_input==None:
+                    return False
+                return True
+            else:
+                return False
+        except:
+            print ('error')
+
+    #主函数
+    def main(self):
+        print (self.getCurrentTime(), u"Hi，FDU-net自动登陆脚本正在运行")
+        while True:
+            can_connect = self.canConnect()
+            if not can_connect:
+                print (self.getCurrentTime(),u"断网了...")
+                try:
+                    self.login()
+                except:
+                    print(self.getCurrentTime(), u"浏览器出了bug")
+                finally:
+                    time.sleep(1)
+                    if self.canConnect():
+                        print(self.getCurrentTime(), u"重新登陆成功! 将在后台运行，保证网络断开时重连。")
+                        time.sleep(2)
+                        if whnd != 0:
+                            ctypes.windll.user32.ShowWindow(whnd, 0)
+                            ctypes.windll.kernel32.CloseHandle(whnd)
+                    else:
+                        print(self.getCurrentTime(), u"登陆失败，再来一次")
+            else:
+                print (self.getCurrentTime(), u"宁已连接网络... 将在后台运行，保证网络断开时重连。")
+                time.sleep(2)
+                if whnd != 0:
+                    ctypes.windll.user32.ShowWindow(whnd, 0)
+                    ctypes.windll.kernel32.CloseHandle(whnd)
+                time.sleep(10)
+            time.sleep(1)
+
+login = Login()
+login.main()
